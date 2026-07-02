53d70abc6edbcd1d92d24b9137fb765aa52efeae
Trevor Fitzgerald <fitztrev@users.noreply.github.com>
2026-07-02 14:24:11 -0400

    link to CMS page

--- modules/tournament/src/main/ui/TournamentShow.scala
+++ modules/tournament/src/main/ui/TournamentShow.scala
@@ -157,7 +157,14 @@ final class TournamentShow(helpers: Helpers, gathering: GatheringUi)(
             tour.description.map: d =>
               st.section(cls := "description")(markdownLinksOrRichText(d)),
             tour.payouts.map: payouts =>
-              st.section(cls := "description")(p(strong("Prizes: "), payouts)),
+              st.section(cls := "description")(
+                p(
+                  strong(
+                    a(href := routes.Cms.lonePage(lila.core.id.CmsPageKey("lichess-prizes")))("Prizes: ")
+                  ),
+                  payouts
+                )
+              ),
             gathering.verdicts(verdicts, tour.perfType, tour.isEnterable),
             List(
               tour.noBerserk.option(
